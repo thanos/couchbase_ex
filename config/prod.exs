c3435e7b59ee03cd2317ca115622f6425d8e2df0
thanos <thanosv@gmail.com>
2025-10-24 20:12:23 -0400

    closed #16

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ import Config
 # Production configuration
 # In production, these should be set via environment variables
 get_int_env = fn var, default ->
-  case Integer.parse(System.get_env(var)) do
+  case Integer.parse(System.get_env(var, default)) do
     {i, ""} -> i
     _ -> default
   end
